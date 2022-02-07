5c0d543dfee3d14a6b93af67ef714ebcd75f6765
SashaYanchenko <s.yanchenko190599@gmail.com>
2022-02-07 23:03:19 +0200

    Добавил часть кода, что бы можно было вводить размерность матрицы

--- loop/src/main/java/loop.java
+++ loop/src/main/java/loop.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
+
 public class loop {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x = 0;
+        boolean result = true;
+        while (result == true) {
+            x = sc.nextInt();
+            if (x < 4) {
+                System.out.println("Размер матрицы должен быть больше 3");
+                continue;
+            }
+            result = false;
+        }
 
         System.out.println();
         System.out.println("_____________________________________");
         System.out.println();
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -16,22 +30,11 @@ public class loop {
         System.out.println("_____________________________________");
         System.out.println();
 
-//        Вывод ^
-//        * * * * * * * * * *
-//        * * * * * * * * * *
-//        * * * * * * * * * *
-//        * * * * * * * * * *
-//        * * * * * * * * * *
-//        * * * * * * * * * *
-//        * * * * * * * * * *
-//        * * * * * * * * * *
-//        * * * * * * * * * *
-//        * * * * * * * * * *
 
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i == 0 || i == 9 || j == 0 || j == 9) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (i == 0 || i == x - 1 || j == 0 || j == x - 1) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
@@ -43,22 +46,11 @@ public class loop {
         System.out.println("_____________________________________");
         System.out.println();
 
-//        Вывод ^
-//        * * * * * * * * * *
-//        *                 *
-//        *                 *
-//        *                 *
-//        *                 *
-//        *                 *
-//        *                 *
-//        *                 *
-//        *                 *
-//        * * * * * * * * * *
 
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i == 0 || j == 0 || i + j == 9) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (i == 0 || j == 0 || i + j == x - 1) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
@@ -71,103 +63,11 @@ public class loop {
         System.out.println("_____________________________________");
         System.out.println();
 
-//        Вывод ^
-//        * * * * * * * * * *
-//        *               *
-//        *             *
-//        *           *
-//        *         *
-//        *       *
-//        *     *
-//        *   *
-//        * *
-//        *
 
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i == 9 || j == 0 || i == j) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.println("_____________________________________");
-        System.out.println();
-
-//        Вывод ^
-//        *
-//        * *
-//        *   *
-//        *     *
-//        *       *
-//        *         *
-//        *           *
-//        *             *
-//        *               *
-//        * * * * * * * * * *
-
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i == 9|| j == 9 || i + j == 9) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.println("_____________________________________");
-        System.out.println();
-
-//        Вывод ^
-//                          *
-//                        * *
-//                      *   *
-//                    *     *
-//                  *       *
-//                *         *
-//              *           *
-//            *             *
-//          *               *
-//        * * * * * * * * * *
-
-
-        for (int i = 0; i < 10; i++){
-            for (int j = 0; j < 10; j++){
-                if (i == 0 || j == 9 || i == j) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.println("_____________________________________");
-        System.out.println();
-
-//        Вывод ^
-//        * * * * * * * * * *
-//          *               *
-//            *             *
-//              *           *
-//                *         *
-//                  *       *
-//                    *     *
-//                      *   *
-//                        * *
-//                          *
-
-
-        for (int i =0; i < 10; i++) {
-            for (int j =0; j < 10; j++) {
-                if (i == j || i +j == 9) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (i == x - 1 || j == 0 || i == j) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
@@ -180,49 +80,10 @@ public class loop {
         System.out.println();
 
 
-//        Вывод ^
-//        *                 *
-//          *             *
-//            *         *
-//              *     *
-//                * *
-//                * *
-//              *     *
-//            *         *
-//          *             *
-//        *                 *
 
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i == 0 || i == j && i < 5 || i + j == 9 && j > 4) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.println("_____________________________________");
-        System.out.println();
-
-//        Вывод ^
-//        * * * * * * * * * *
-//          *             *
-//            *         *
-//              *     *
-//                * *
-//
-//
-//
-//
-//
-
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i == 9 || i == j && i > 4 || i + j == 9 && j < 5) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (i == x - 1 || j == x - 1 || i + j == x - 1) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
@@ -235,17 +96,69 @@ public class loop {
         System.out.println();
 
 
-//        Вывод ^
-//
-//
-//
-//
-//
-//        * *
-//      *     *
-//    *         *
-//  *             *
-//* * * * * * * * * *
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (i == 0 || j == x - 1 || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("_____________________________________");
+        System.out.println();
+
+
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (i == j || i + j == x - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("_____________________________________");
+        System.out.println();
+
+
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (i == 0 || i == j && i < x / 2 || i + j == x - 1 && j > x / 2 - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("_____________________________________");
+        System.out.println();
+
+
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (i == x - 1 || i == j && i > x / 2 - 1 || i + j == x - 1  && j < x / 2) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("_____________________________________");
+        System.out.println();
+
 
 
 
